6a1a6d0e93de76498528a3279f473cfb794a928f
Luis Felipe <luisfelipe@cohete.net>
2026-01-31 21:46:21 -0500

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,20 @@
+
+async function loader(){
+  const path="/api/home-page";
+  const BASE_URL="http://localhost:1337";
+  const url=new URL(path,BASE_URL);
+
+  const response=await fetch(url.href);
+  const data=await response.json();
+  console.log(data);
+  return {...data.data};
+}
+
+export default async function HomeRoute() {
+  const data= await loader();
+  console.log("this is the data:"+data);
+  return (<div>
+    <h1>{data.title}</h1>
+    <p>{data.description}</p>
+  </div>);
+}
